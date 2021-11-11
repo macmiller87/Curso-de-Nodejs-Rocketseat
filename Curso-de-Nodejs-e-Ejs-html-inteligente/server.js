bcae82efadde59f048eab9d1d9770218e98fee95
Macmiller <macamagolf@gmail.com>
2021-11-11 04:21:56 -0300

    update file

--- Curso-de-Nodejs-e-Ejs-html-inteligente/server.js
+++ Curso-de-Nodejs-e-Ejs-html-inteligente/server.js
@@ -5,7 +5,40 @@ app.set("view engine", "ejs"); // Aqui está sendo passado os parametros para o 
 
 app.get("/", function (req, res) { // Aqui está sendo criado a rota do arquivo para ser renderizado, com os parametros req, de requisição e res de resposta.
 
-    res.render("pages/index");
+    const items = [ // Aqui foi criado um array de objetos, para ser usado no index.ejs
+        {
+            title: "D",
+            message: " esenvolver aplicações de forma fácil",
+        },
+        {
+            title: "E",
+            message: " JS usa Javascript para renderizar HTML",
+        },
+        {
+            title: "M",
+            message: " uito fácil",
+        },
+        {
+            title: "A",
+            message: " morzinho",
+        },
+        {
+            title: "I",
+            message: " nstall EJS",
+        },
+        {
+            title: "S",
+            message: " intaxe simples",
+        }
+    ];
+
+    const subtitle = "Uma linguagem de modelagem para criação de páginas HTML utilizando JS !";
+
+    res.render("pages/index", { 
+
+        qualitys: items, // aqui está sendo passado o array acima para ser renderizado no arquivo index.ejs
+        subtitle: subtitle,
+    });
 });
 
 app.get("/sobre", function (req, res) { 
